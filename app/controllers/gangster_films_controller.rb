155b77d0577e705aef179fa4d0a6ff49da08676c
Lionel Mason <capitalcookhouse@gmail.com>
2022-11-02 22:48:34 -0400

    creates seed data for each film

--- app/controllers/gangster_films_controller.rb
+++ app/controllers/gangster_films_controller.rb
@@ -1,7 +1,7 @@
 class GangsterFilmsController < ApplicationController
    
     get '/gangster_films' do 
-        GangsterFilm.all.to_json
+        GangsterFilm.all.to_json(include: [:gangster_films]) #includes all the gangster films
     end
     # Add your routes here
     
